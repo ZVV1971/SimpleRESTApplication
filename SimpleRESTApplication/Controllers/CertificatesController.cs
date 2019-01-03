84eb466fb13105105e41b70f81a7950eae74949a
Zakharenkov Vladimir <vvzakharenkov@outlook.com>
2019-01-03 23:54:35 +0300

    PDF changes introduced

--- SimpleRESTApplication/Controllers/CertificatesController.cs
+++ SimpleRESTApplication/Controllers/CertificatesController.cs
@@ -54,6 +54,7 @@ namespace SimpleRESTApplication.Controllers
         }
 
         [System.Web.Http.HttpGet]
+        
         public HttpResponseMessage Get()
         {
             HttpResponseMessage res = null;
@@ -95,13 +96,20 @@ namespace SimpleRESTApplication.Controllers
             //check if certificate is 8 characters long and digits only
             //Taganrog ceritificates detection
             if (str[0].Length == 8 && str[0].All((x) => x >= '0' && x <= '9'))
-                realPath = HostingEnvironment.MapPath("/Certificates") + @"\20" + str[0][1] + str[0][2] + @"\ТМЗ\" + str[0] + ".pdf";
-            else //non-TMZ certificate have to look for the date
+                realPath = HostingEnvironment.MapPath("/Certificates") + @"\20" + str[0][1] + str[0][2] 
+                        + @"\ТМЗ\" + str[0] + ".pdf";
+            //non-TMZ certificate 
+            else if (str[0].Contains('/')) //
             {
+                //KTZ -- have to look for the date
                 Match mtch = Regex.Match(str[1], pattern, RegexOptions.IgnoreCase);
-                realPath = HostingEnvironment.MapPath("/Certificates") + ((mtch.Captures[0].Value.Length == 2) ?
-                        @"\20" + mtch.Captures[0].Value + @"\КТЗ\" : @"\" + mtch.Captures[0].Value + @"\КТЗ\") 
+                try // in case wrong year is provided (not 2 nor 4 digits)
+                {
+                    realPath = HostingEnvironment.MapPath("/Certificates") + ((mtch.Captures[0].Value.Length == 2) ?
+                        @"\20" + mtch.Captures[0].Value + @"\КТЗ\" : @"\" + mtch.Captures[0].Value + @"\КТЗ\")
                         + str[0].Replace('/', '_') + ".pdf";
+                }
+                catch { }
             }
             return realPath;
         }
